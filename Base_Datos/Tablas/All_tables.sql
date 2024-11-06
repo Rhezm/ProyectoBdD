a090dfe82b568e4001894dee9d841656931b92cb
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2024-11-06 18:46:25 -0300

    Update All_tables.sql
    Se agregaron las PK, faltan las FK

--- Base_Datos/Tablas/All_tables.sql
+++ Base_Datos/Tablas/All_tables.sql
@@ -4,23 +4,29 @@ create table TB_Nueva_Venta_JRSG (
     cantidad_producto number,
     precio_producto number,
     stock_producto number,
+
+    constraint PK_TB_Nueva_Venta_JRSG primary key (codigo_producto)
 );
 
-create table TB_Clientes_JRSG (
+create table TB_Cliente_JRSG (
     id_cliente number,
     rut_cliente number,
     nombre_cliente varchar2(80),
     apellido1_cliente varchar2(50),
     apellido2_cliente varchar2(50),
     telefono_cliente number, 
-    email_cliente varchar2(150)
+    email_cliente varchar2(150),
+
+    constraint PK_TB_Cliente_JRSG primary key (id_cliente)
 );
 
-create table TB_Proveedores_JRSG (
+create table TB_Proveedor_JRSG (
     id_proveedor number,
     nombre_proveedor varchar2(150),
     telefono_proveedor number,
-    email_proveedor varchar2(150)
+    email_proveedor varchar2(150),
+
+    constraint PK_TB_Proveedor_JRSG primary key (id_proveedor)
 );
 
 create table TB_Venta_JRSG (
@@ -28,7 +34,9 @@ create table TB_Venta_JRSG (
     id_cliente number,
     metodo_pago varchar2(20),
     total_venta number,
-    sucursal number
+    sucursal number,
+
+    constraint PK_TB_VEnta_JRSG primary key (id_venta)
 );
 
 create table TB_Configuracion_JRSg (
