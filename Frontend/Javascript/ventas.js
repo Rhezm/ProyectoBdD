9034072a44773ecaf7f574dec47acf1eec149311
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-09 06:52:33 +0000

    Update

--- Frontend/Javascript/ventas.js
+++ Frontend/Javascript/ventas.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const content = document.getElementById('content');
     content.innerHTML = `
         <h1>Registros de Ventas</h1>
-         <p>Aquí puedes ver la información de contacto.</p>
+         <p>Aquí puedes ver la información de tus ventas.</p>
         <!-- Agrega más contenido específico para "Contacto" aquí -->
     `;
 });
