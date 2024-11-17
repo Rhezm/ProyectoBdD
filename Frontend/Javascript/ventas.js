ce891b2f702a8610331fd2f4e1fcfc79a343e162
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-17 02:46:59 -0300

    Update ventas.js

--- Frontend/Javascript/ventas.js
+++ Frontend/Javascript/ventas.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
         <p>Aquí puedes ver la información de tus ventas.</p>
         <button id="mostrarTablaBtn">Mostrar Tabla</button>
         <div id="tablaContenedor" style="display:none;">
-            <table border="1">
+            <table border="0">
                 <thead>
                     <tr>
                         <th>ID</th>
