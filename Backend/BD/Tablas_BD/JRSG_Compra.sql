0182e8431060ca66419f41cebb886c65ea8c93c9
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2024-11-17 18:08:37 -0300

    Delete Backend/BD/Tablas_BD directory

--- Backend/BD/Tablas_BD/JRSG_Compra.sql
+++
@@ -1,9 +0,0 @@
-create table JRSG_Compra (
-    id_compra number,
-    id_proveedor number,
-    cantidad_suministro number,
-    fecha_suministro date,
-
-    constraint PK_JRSG_Compra primary key (id_compra),
-    constraint FK_JRSG_Proveedor foreign key (id_proveedor) references JRSG_Proveedor (id_proveedor)
-);
