e36c2be06105271f6befbc2999c04abaeef55275
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2024-11-17 18:08:21 -0300

    Delete Backend/BD/Secuencias directory

--- Backend/BD/Secuencias/JRSG_Sec_Generar_ID_Clientes.sql
+++
@@ -1,9 +0,0 @@
-create sequence JRSG_Sec_Generar_ID_Clientes
-    start with 1 
-    increment by 1
-    minvalue 1
-    maxvalue 1000
-    nocache
-    nocycle;
-
-    --- Compila correctamnte en SQL Developer
