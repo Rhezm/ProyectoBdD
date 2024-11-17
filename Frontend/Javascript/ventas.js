4a67b6f64c669c79eb9995937cdd68057b39ede8
sofia-espinosa <sofiasolis471@gmail.com>
2024-11-17 01:08:55 -0300

    Update ventas.js

--- Frontend/Javascript/ventas.js
+++ Frontend/Javascript/ventas.js
@@ -4,5 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
         <h1>Registros de Ventas</h1>
          <p>Aquí puedes ver la información de tus ventas.</p>
         <!-- Agrega más contenido específico para "Contacto" aquí -->
-    `;
+        document.getElementById('generarReporte').addEventListener('click', function () {
+    window.open('reporte.php', '_blank');
 });
