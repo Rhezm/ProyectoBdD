e5c3807a92f925bbdf27160dd120cc43e818588d
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-17 02:26:44 -0300

    Update ventas.js

--- Frontend/Javascript/ventas.js
+++ Frontend/Javascript/ventas.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Crear el contenido HTML
-    const contenidoHTML = `
+    const content = document.getElementById('content');
+    content.innerHTML = `
         <h1>Registros de Ventas</h1>
         <p>Aquí puedes ver la información de tus ventas.</p>
         <button id="mostrarTablaBtn">Mostrar Tabla</button>
