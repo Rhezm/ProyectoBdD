da6ef99056507a92bb7a767d37db6a1d3b72c619
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-17 04:32:07 +0000

    Update

--- Frontend/Javascript/ventas.js
+++ Frontend/Javascript/ventas.js
@@ -2,9 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const content = document.getElementById('content');
     content.innerHTML = `
         <h1>Registros de Ventas</h1>
-         <p>Aquí puedes ver la información de tus ventas.</p>
+        <p>Aquí puedes ver la información de tus ventas.</p>
         <!-- Agrega más contenido específico para "Contacto" aquí -->
         <button id="generarReporte">Generar Reporte PDF</button>
-        document.getElementById('generarReporte').addEventListener('click', function () {
-    window.open('reporte.php', '_blank');
+    `;
+
+    document.getElementById('generarReporte').addEventListener('click', function () {
+        window.open('reporte.php', '_blank');
+    });
 });
