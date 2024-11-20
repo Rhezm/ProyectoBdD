82a63211bf626e3f217bb2bf43f4f8474a5d1a56
TITO <117479455+Titosky21@users.noreply.github.com>
2024-11-20 12:17:57 -0300

    Create JRSG_TRIG_MAYUS_EMPLEADOS.sql

---
+++ Backend/BD/Trigger's/JRSG_TRIG_MAYUS_EMPLEADOS.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE TRIGGER JRSG_TRIG_MAYUS_EMPLEADOS
+BEFORE INSERT ON JRSG_EMPLEADO 
+FOR EACH ROW
+BEGIN
+    :NEW.nombre_empleado := UPPER(:NEW.nombre_empleado);
+    :NEW.apellido1_empleado := UPPER(:NEW.apellido1_empleado);
+    :NEW.apellido2_empleado := UPPER(:NEW.apellido2_empleado);
+    :NEW.email_empleado := UPPER(:NEW.email_empleado);
+END;
