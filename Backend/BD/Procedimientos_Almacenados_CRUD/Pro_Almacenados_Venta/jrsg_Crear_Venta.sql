35ae7f2ace8f8c7637b44381526b6dc0e32ebfa3
TITO <117479455+Titosky21@users.noreply.github.com>
2024-11-20 20:35:07 -0300

    Create jrsg_Crear_Venta.sql

---
+++ Backend/BD/Procedimientos_Almacenados_CRUD/Pro_Almacenados_Venta/jrsg_Crear_Venta.sql
@@ -0,0 +1,20 @@
+create or replace procedure JRSG_Pro_Crear_Venta(
+    id_cliente_v in number,
+    fecha_venta_v date
+
+) is
+    lock table JRSG_Venta in row exclusive mode;
+
+    insert into JRSG_Venta (id_venta, id_cliente, fecha_venta_v)
+    values (JRSG_Sec_Generar_ID_Venta.nextval, id_cliente_v, fecha_venta_v);
+
+    commit;
+        dbms_output.put_line ('Venta con ID: '|| JRSG_Sec_Generar_ID_Venta.currval ||' se creo correctamente.');
+
+    exception
+        when storage_error then
+            raise_application_error (-6500, 'Error en el almacenamiento: No hay sufieciente espacio en el sistema.');
+        when others then
+            raise_application_error (-20004, 'Se ha producido un error inesperado.'|| sqlerrm);
+    rollback;    
+end;
