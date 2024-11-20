1e68e8ae608823fac11f404b0d41791009cdae4f
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2024-11-20 02:50:06 -0300

    Delete Backend/BD/Secuencias/JRSG_Sec_Generar_ID_Proveedores.sql

--- Backend/BD/Secuencias/JRSG_Sec_Generar_ID_Proveedores.sql
+++
@@ -1,9 +0,0 @@
-create sequence JRSG_Sec_Generar_ID_Proveedores
-    start with 1 
-    increment by 1
-    minvalue 1
-    maxvalue 1000
-    nocache
-    nocycle;
-
-    --- Compila correctamnte en SQL Developer
