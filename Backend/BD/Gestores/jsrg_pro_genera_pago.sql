1a014a201aec4d00a4d0abbd88ac6d1a17bc0e05
TITO <117479455+Titosky21@users.noreply.github.com>
2024-11-20 12:36:09 -0300

    Update jsrg_pro_genera_pago.sql

--- Backend/BD/Gestores/jsrg_pro_genera_pago.sql
+++ Backend/BD/Gestores/jsrg_pro_genera_pago.sql
@@ -1,11 +1,11 @@
 create or replace procedure jrsg_pro_genera_pago(
 	p_id_venta number,
 	p_monto_pago number
-	)
+)
 as
 begin
 	select id_producto, p_monto_pago := sum(dvp.cantidad * p.precio)
 	from jrsg_detalle_venta_producto
 	inner join producto p on dvp.id_producto = p.id_producto
-	where dvp.id_venta = p_id_venta
+	where dvp.id_venta = p_id_venta;
 end;
