c7969d0cec7a248c9f4713112ff038ec19328332
TITO <117479455+Titosky21@users.noreply.github.com>
2024-11-20 02:21:39 -0300

    Delete Backend/BD/Tablas_BD directory

--- Backend/BD/Tablas_BD/JRSG_Cargo.sql
+++
@@ -1,7 +0,0 @@
-create table JRSG_Cargo ( --- Nueva Tabla.
-    id_cargo number,
-    nombre_cargo varchar2(20),
-    salario number,
-
-    constraint PK_JRSG_Cargo primary key (id_cargo)
-);
--- Backend/BD/Tablas_BD/JRSG_Detalle_Venta_Producto.sql
+++
@@ -1,9 +0,0 @@
-create table JRSG_Detalle_Venta_Producto ( --- Se actualizo, se agrego cantidad.
-    id_producto number,
-    id_venta number,
-    cantidad number,
-
-    constraint PK_JRSG_Detalle_Venta_Producto primary key (id_producto, id_venta),
-    constraint FK_JRSG_Venta2 foreign key (id_venta) references JRSG_Venta (id_venta),
-    constraint FK_JRSG_Producto1 foreign key (id_producto) references JRSG_Producto (id_producto)
-);
