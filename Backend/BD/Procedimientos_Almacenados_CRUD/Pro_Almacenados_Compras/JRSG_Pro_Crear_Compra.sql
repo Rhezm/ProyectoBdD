40d29a530a86308e689f7b27f4c01a7158472f02
TITO <117479455+Titosky21@users.noreply.github.com>
2024-11-21 05:44:14 -0300

    Update JRSG_Pro_Crear_Compra.sql

--- Backend/BD/Procedimientos_Almacenados_CRUD/Pro_Almacenados_Compras/JRSG_Pro_Crear_Compra.sql
+++ Backend/BD/Procedimientos_Almacenados_CRUD/Pro_Almacenados_Compras/JRSG_Pro_Crear_Compra.sql
@@ -1,21 +1,22 @@
-create or replace procedure JRSG_Pro_Crear_Compra (
-    detalle_compra_p in varchar2,
-    monto_compra_p in number,
-    fecha_compra_p in date
+create or replace NONEDITIONABLE procedure JRSG_Pro_Crear_Compra (
+    monto_compra_p in number default null
 ) is
-    begin
-        lock table JRSG_Compra in row exclusive mode;
-        
-        insert into JRSG_Compra (id_compra, detalle_compra, monto_compra, fecha_compra) 
-        values (JRSG_Sec_Generar_ID_Compras.nextval, detalle_compra_p, monto_compra_p, fecha_compra_p);
+    v_fecha_compra date;
+begin
+    lock table JRSG_Compra in row exclusive mode;
+    
+    v_fecha_compra := SYSDATE;
+    
+    insert into JRSG_Compra(id_compra, monto_compra, fecha_compra) 
+    values (JRSG_Sec_Generar_ID_Compras.nextval, monto_compra_p, v_fecha_compra);
 
-        commit;
-        dbms_output.put_line ('La orden de compra ID: ' || JRSG_Sec_Generar_ID_Compras.currval || ' se realizo correctamente');
+    commit;
+    dbms_output.put_line ('La orden de compra ID: ' || JRSG_Sec_Generar_ID_Compras.currval || ' se realizo correctamente');
 
-        exception
-            when storage_error then
-                raise_application_error (-6500, 'Error en el almacenamiento: No hay sufieciente espacio en el sistema.');
-            when others then
-                raise_application_error (-20004, 'Se ha producido un error inesperado.'|| sqlerrm);
-        rollback;    
-    end;
+    exception
+        when storage_error then
+            raise_application_error (-6500, 'Error en el almacenamiento: No hay sufieciente espacio en el sistema.');
+        when others then
+            raise_application_error (-20004, 'Se ha producido un error inesperado.'|| sqlerrm);
+    rollback;    
+end;
