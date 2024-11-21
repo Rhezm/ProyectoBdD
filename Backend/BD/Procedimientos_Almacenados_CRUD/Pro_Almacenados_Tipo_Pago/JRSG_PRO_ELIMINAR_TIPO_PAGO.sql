96de1b154fe20a9d82fe0898a5bbd3bf89eb8a6b
TITO <117479455+Titosky21@users.noreply.github.com>
2024-11-20 23:07:24 -0300

    Create JRSG_PRO_ELIMINAR_TIPO_PAGO.sql

---
+++ Backend/BD/Procedimientos_Almacenados_CRUD/Pro_Almacenados_Tipo_Pago/JRSG_PRO_ELIMINAR_TIPO_PAGO.sql
@@ -0,0 +1,22 @@
+create or replace NONEDITIONABLE PROCEDURE JRSG_PRO_ELIMINAR_TIPO_PAGO(
+    id_tipo_pago_tp number
+)IS
+contador number;
+begin
+    select count(*) into contador from JRSG_TIPO_PAGO where id_tipo_pago = id_tipo_pago_tp;
+
+    if (contador > 0) then
+        lock table JRSG_TIPO_PAGO in row exclusive mode;
+
+        delete from JRSG_TIPO_PAGO where id_tipo_pago = id_tipo_pago_tp;
+        dbms_output.put_line ('El Tipo de Pago ID: '|| id_tipo_pago_tp ||' se elimino correctamente del sistema.');
+        commit;
+    else
+        dbms_output.put_line ('El Tipo de Pago ID: '|| id_tipo_pago_tp ||' no se encontro en el sistema.');
+    end if;
+
+    exception
+        when others then
+            raise_application_error (-20004, 'Error inesperado: '|| sqlerrm);
+    rollback;
+end;
