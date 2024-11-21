c2bef93fadd4184aa85c4f4526fa719cef947905
TITO <117479455+Titosky21@users.noreply.github.com>
2024-11-21 05:49:02 -0300

    Update JRSG_Pro_Mostrar_Compra.sql

--- Backend/BD/Procedimientos_Almacenados_CRUD/Pro_Almacenados_Compras/JRSG_Pro_Mostrar_Compra.sql
+++ Backend/BD/Procedimientos_Almacenados_CRUD/Pro_Almacenados_Compras/JRSG_Pro_Mostrar_Compra.sql
@@ -3,23 +3,21 @@ create or replace procedure JRSG_Pro_Mostrar_Compra (
 ) is
     contador number;
     v_id_compra JRSG_Compra.id_compra%type;
-    v_detalle_compra JRSG_Compra.detalle_compra%type;
     v_monto_compra JRSG_Compra.monto_compra%type;
     v_fecha_compra JRSG_Compra.fecha_compra%type;
 
     begin
-        select count(id_compra) into contador from JRSG_Compra;
+        select count(*) into contador from JRSG_Compra where id_compra = id_compra_p;
 
         if (contador > 0) then
-            select * into v_id_compra, v_detalle_compra, v_monto_compra, v_fecha_compra
+            select * into v_id_compra, v_monto_compra, v_fecha_compra
             from JRSG_Compra where id_compra = id_compra_p;
 
             dbms_output.put_line('Informacion Compra ID: ' || id_compra_p || CHR(10) ||
-                              'Detalle Compra: ' || v_detalle_compra || CHR(10) || 
                               'Monto Compra: ' || v_monto_compra || CHR(10) || 
                               'Fecha Compra: ' || v_fecha_compra);
         else
-            dbms_output.put_line ('Promocion ID: '|| id_compra_p ||' no encontrado en el sistema.');
+            dbms_output.put_line ('La compra id: '|| id_compra_p ||' no se encuentra en el sistema.');
         end if;
 
         exception
