374e6c2e194160904e5f2dd0f51ddd376efaa2bd
TITO <117479455+Titosky21@users.noreply.github.com>
2024-11-21 05:43:47 -0300

    Update JRSG_Pro_Actualizar_Compra.sql

--- Backend/BD/Procedimientos_Almacenados_CRUD/Pro_Almacenados_Compras/JRSG_Pro_Actualizar_Compra.sql
+++ Backend/BD/Procedimientos_Almacenados_CRUD/Pro_Almacenados_Compras/JRSG_Pro_Actualizar_Compra.sql
@@ -1,6 +1,5 @@
-create or replace procedure JRSG_Pro_Actualizar_Compra (
+create or replace NONEDITIONABLE procedure JRSG_Pro_Actualizar_Compra (
     id_compra_p in number,
-    detalle_compra_p in varchar2,
     monto_compra_p in number,
     fecha_compra_p in date
 ) is
@@ -11,8 +10,7 @@ create or replace procedure JRSG_Pro_Actualizar_Compra (
         if (contador > 0) then
             lock table JRSG_Compra in row exclusive mode;
 
-            update JRSG_Compra set detalle_compra = detalle_compra_p,
-                                    monto_compra = monto_compra_p,
+            update JRSG_Compra set  monto_compra = monto_compra_p,
                                     fecha_compra = fecha_compra_p where id_compra = id_compra_p;
 
             dbms_output.put_line ('Compra ID: ' || id_compra_p || ' se ha actualizado correctamente.');
