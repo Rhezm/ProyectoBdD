83626483f744e98d1f0b91d839e357b7042a50dd
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2024-11-21 20:15:42 -0300

    Nuevo atributo - nombre_producto

--- Backend/BD/Tablas_BD/JRSG_Detalle_Venta_Producto.sql
+++ Backend/BD/Tablas_BD/JRSG_Detalle_Venta_Producto.sql
@@ -2,6 +2,7 @@ create table JRSG_Detalle_Venta_Producto ( --- Se actualizo, se agrego cantidad.
     id_producto number,
     id_venta number,
     cantidad number,
+    nombre_producto varchar2(60),
 
     constraint PK_JRSG_Detalle_Venta_Producto primary key (id_producto, id_venta),
     constraint FK_JRSG_Venta2 foreign key (id_venta) references JRSG_Venta (id_venta),
