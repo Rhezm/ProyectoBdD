cb40f432b9493ceee20646e9ad28a33264b44842
TITO <117479455+Titosky21@users.noreply.github.com>
2024-11-20 22:29:42 -0300

    Update JRSG_Ubicacion_Bodega.sql

--- Backend/BD/Tablas_BD/JRSG_Ubicacion_Bodega.sql
+++ Backend/BD/Tablas_BD/JRSG_Ubicacion_Bodega.sql
@@ -1,7 +1,9 @@
 create table JRSG_Ubicacion_Bodega (
     id_ubicacion number,
+    id_producto number,
     nro_estante number,
     nivel number,
 
-    constraint PK_JRSG_Ubicacion_Bodega primary key (id_ubicacion)
+    constraint PK_JRSG_Ubicacion_Bodega primary key (id_ubicacion),
+    constraint FK_JRSG_Ubicacion_Bodega_Id_Producto foreign key(id_producto) references JRSG_PRODUCTO(id_producto)
 );
