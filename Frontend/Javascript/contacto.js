d427ed6747d2e37ac8ae4cfc42deca63d96157fb
sofia-espinosa <sofiasolis471@gmail.com>
2024-11-21 00:02:50 -0300

    Update contacto.js

--- Frontend/Javascript/contacto.js
+++ Frontend/Javascript/contacto.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const content = document.getElementById('content');
-     content.innerHTML = `
-        <h1>Contacto</h1>
-        <p>Aquí puedes ver la información de contacto.</p>
-        <!-- Agrega más contenido específico para "Contacto" aquí -->
+    content.innerHTML = `
+      <h1>Contacto</h1>
+      <p>Información de contacto.</p>
+      <p><strong>Cordoneria Andrades</strong> </p>
+      <p><strong>Representante legal:</strong> Odette Patricia Andrades Oyarzun</p>
+      <p><strong>Run:</strong> 11.</p>
+      <p><strong>Ubicación:</strong> 8 oriente entre 1 y 2 sur #1068</p>
+      <p><strong>Teléfono:</strong> +56 9 99798467</p>
+      <p><strong>Email:</strong> pandradeso@gmail.com</p>
+      <!-- Agrega más contenido específico para "Contacto" aquí -->
     `;
 });
