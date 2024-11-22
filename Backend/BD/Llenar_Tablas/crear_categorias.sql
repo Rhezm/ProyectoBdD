48d48e53482ad84befbbd0b3dde45793efe5635b
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-21 21:29:41 -0300

    Create crear_categorias.sql

---
+++ Backend/BD/Llenar_Tablas/crear_categorias.sql
@@ -0,0 +1,16 @@
+begin
+	JRSG_Pro_Crear_Categoria('Hilos');
+	JRSG_Pro_Crear_Categoria('Cordones');
+	JRSG_Pro_Crear_Categoria('Cintas y Listones');
+	JRSG_Pro_Crear_Categoria('Botones');
+	JRSG_Pro_Crear_Categoria('Cremalleras y Cierres');
+	JRSG_Pro_Crear_Categoria('Encajes');
+	JRSG_Pro_Crear_Categoria('Pasamanería');
+	JRSG_Pro_Crear_Categoria('Adhesivos y Pegamentos');
+	JRSG_Pro_Crear_Categoria('Agujas');
+	JRSG_Pro_Crear_Categoria('Alfileres');
+	JRSG_Pro_Crear_Categoria('Lanas');
+	JRSG_Pro_Crear_Categoria('Hilos para Tejer');
+	JRSG_Pro_Crear_Categoria('Instrumentos de Medición');
+	JRSG_Pro_Crear_Categoria('Decoraciones');
+end;
