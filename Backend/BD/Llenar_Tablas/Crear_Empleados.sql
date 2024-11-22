e2cb79a19c51ee070387ae1bda724123a5e863d0
TITO <117479455+Titosky21@users.noreply.github.com>
2024-11-21 21:52:05 -0300

    Create Crear_Empleados.sql

---
+++ Backend/BD/Llenar_Tablas/Crear_Empleados.sql
@@ -0,0 +1,6 @@
+begin
+	JRSG_Pro_Crear_Empleado('Roberto', 'Barros', 'Gutiérrez', 111111111, 'roberto@gmail.com', 'roberto');
+	JRSG_Pro_Crear_Empleado('Jeison', 'Salas', 'Coloma', 222222222, 'roberto@gmail.com', 'jeison');
+	JRSG_Pro_Crear_Empleado('Sofia', 'Espinosa', 'Solís', 333333333, 'roberto@gmail.com', 'sofia');
+	JRSG_Pro_Crear_Empleado('Gustavo', 'Perez', 'Perez', 444444444, 'roberto@gmail.com', 'gustavo');
+end;
