596e68bb88e20cdf6b6dbcb3302ecacede71dcde
TITO <117479455+Titosky21@users.noreply.github.com>
2024-11-21 23:08:47 -0300

    Update JRSG_Pro_Crear_Producto.sql

--- Backend/BD/Procedimientos_Almacenados_CRUD/Pro_Almacenados_Productos/JRSG_Pro_Crear_Producto.sql
+++ Backend/BD/Procedimientos_Almacenados_CRUD/Pro_Almacenados_Productos/JRSG_Pro_Crear_Producto.sql
@@ -1,12 +1,11 @@
-create or replace procedure JRSG_Pro_Crear_Producto (
+create or replace NONEDITIONABLE procedure JRSG_Pro_Crear_Producto (
     id_categoria_p in number default null,
     id_promocion_p in number default null,
     nombre_producto_p in varchar2,
     descripcion_producto_p in varchar2,
     precio_p in number,
     precio_descuento_p in number default null,
-    stock_p in number,
-    proveedor_p in varchar2 default null
+    stock_p in number
 )is
     contador number;
     begin
@@ -17,13 +16,13 @@ create or replace procedure JRSG_Pro_Crear_Producto (
         else
             lock table JRSG_Producto in row exclusive mode;
 
-            insert into JRSG_Producto (id_producto, id_categoria, id_promocion, nombre_producto, descripcion_producto, precio, precio_descuento, stock, proveedor)
-            values (JRSG_Sec_Generar_ID_Productos.nextval, id_categoria_p, id_promocion_p, nombre_producto_p, descripcion_producto_p, precio_p, precio_descuento_p, stock_p, proveedor_p);
-            
+            insert into JRSG_Producto (id_producto, id_categoria, id_promocion, nombre_producto, descripcion_producto, precio, precio_descuento, stock)
+            values (JRSG_Sec_Generar_ID_Productos.nextval, id_categoria_p, id_promocion_p, nombre_producto_p, descripcion_producto_p, precio_p, precio_descuento_p, stock_p);
+
             commit;
             dbms_output.put_line ('Producto con ID: '|| JRSG_Sec_Generar_ID_Productos.currval ||' se creo correctamente.');
         end if;
-        
+
         exception
             when storage_error then
                 raise_application_error (-6500, 'Error en el almacenamiento: No hay sufieciente espacio en el sistema.');
