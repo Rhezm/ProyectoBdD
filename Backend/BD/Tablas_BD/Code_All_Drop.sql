a4433fa25a79be977db55490ff1c4a7659d4164a
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-30 07:46:57 -0300

    Update Code_All_Drop.sql

--- Backend/BD/Tablas_BD/Code_All_Drop.sql
+++ Backend/BD/Tablas_BD/Code_All_Drop.sql
@@ -4,11 +4,8 @@ drop table JRSG_Boleta;
 drop table JRSG_Producto;
 drop table JRSG_Pago;
 drop table JRSG_Venta;
-drop table JRSG_Promocion;
 drop table JRSG_Categoria;
 drop table JRSG_Compra;
 drop table JRSG_Empleado;
 drop table JRSG_Cliente;
 drop table JRSG_Tipo_Pago;
-
---- Compila Correctamente en SQL Oracle: Comprobaciòn 25 nov 2024 - 11:21 am
