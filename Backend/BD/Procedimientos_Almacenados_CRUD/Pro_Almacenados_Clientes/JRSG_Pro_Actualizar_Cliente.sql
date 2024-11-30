d8369eeb8850b9ebc1f883b1abff8f269597fad5
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-30 08:00:26 -0300

    Update JRSG_Pro_Actualizar_Cliente.sql

--- Backend/BD/Procedimientos_Almacenados_CRUD/Pro_Almacenados_Clientes/JRSG_Pro_Actualizar_Cliente.sql
+++ Backend/BD/Procedimientos_Almacenados_CRUD/Pro_Almacenados_Clientes/JRSG_Pro_Actualizar_Cliente.sql
@@ -4,7 +4,6 @@ create or replace procedure JRSG_Pro_Actualizar_Cliente (
     apellido1_cliente_p in varchar2 default null,
     apellido2_cliente_p in varchar2 default null,
     telefono_cliente_p in number default null,
-    direccion_cliente_p in varchar2 default null,
     email_cliente_p in varchar2 default null
 ) is
     contador number;
@@ -19,7 +18,6 @@ create or replace procedure JRSG_Pro_Actualizar_Cliente (
                                     apellido1_cliente = apellido1_cliente_p,
                                     apellido2_cliente = apellido2_cliente_p,
                                     telefono_cliente = telefono_cliente_p,
-                                    direccion_cliente = direccion_cliente_p,
                                     email_cliente = email_cliente_p where id_cliente = id_cliente_p;
             dbms_output.put_line ('Los datos del cliente con ID: ' || id_cliente_p || ' se han actualizado');
             commit; 
