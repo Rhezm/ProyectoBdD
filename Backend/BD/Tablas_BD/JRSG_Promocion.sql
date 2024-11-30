0775a50c1e6d4e68d3cdda23a8f4a9fb9690b0c9
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-30 07:46:34 -0300

    Delete Backend/BD/Tablas_BD/JRSG_Promocion.sql

--- Backend/BD/Tablas_BD/JRSG_Promocion.sql
+++
@@ -1,9 +0,0 @@
-create table JRSG_Promocion (
-    id_promocion number,
-    nombre_promocion in varchar2(25),
-    descuento number,
-    fecha_inicio date,
-    fecha_fin date,
-
-    constraint PK_JRSG_Promocion primary key (id_promocion)
-);
