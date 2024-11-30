ce2be34bc3867a2dac7f70e16780c6e9e5aa057c
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-30 07:42:27 -0300

    Update JRSG_Pro_Obtener_Menos_Vendidos.sql

--- Backend/BD/Registros /JRSG_Pro_Obtener_Menos_Vendidos.sql
+++ Backend/BD/Registros /JRSG_Pro_Obtener_Menos_Vendidos.sql
@@ -1,13 +1,18 @@
-create or replace PROCEDURE JRSG_Pro_Obtener_Menos_Vendidos (
-    p_cursor OUT SYS_REFCURSOR
+create or replace PROCEDURE jrsg_pro_obtener_menos_vendidos (
+  p_cursor OUT SYS_REFCURSOR
 ) AS
 BEGIN
-    OPEN p_cursor FOR
-        SELECT p.id_producto AS a, p.nombre_producto AS aa, SUM(dvp.cantidad) AS total_vendido
-        FROM jrsg_detalle_venta_producto dvp
-        INNER JOIN jrsg_producto p ON dvp.id_producto = p.id_producto
-        GROUP BY p.id_producto, p.nombre_producto
-        ORDER BY total_vendido ASC
-        FETCH FIRST 10 ROWS ONLY;
-    CLOSE p_cursor;
+  OPEN p_cursor FOR
+    SELECT p.id_producto AS a, p.nombre_producto AS aa, SUM(dvp.cantidad) AS total_vendido
+    FROM jrsg_detalle_venta_producto dvp
+    INNER JOIN jrsg_producto p ON dvp.id_producto = p.id_producto
+    GROUP BY p.id_producto, p.nombre_producto
+    ORDER BY total_vendido ASC
+    FETCH FIRST 10 ROWS ONLY;
+    exception
+        when storage_error then
+            raise_application_error (-6500, 'Error en el almacenamiento: No hay sufieciente espacio en el sistema.');
+        when others then
+            raise_application_error (-20004, 'Se ha producido un error inesperado' || sqlerrm);
+    rollback;
 END;
