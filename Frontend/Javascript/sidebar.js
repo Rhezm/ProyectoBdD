9a198968ff149791e4d13a371a5b33617b5ae1bd
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-30 08:15:39 -0300

    Update sidebar.js

--- Frontend/Javascript/sidebar.js
+++ Frontend/Javascript/sidebar.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const sidebarPlaceholder = document.getElementById('sidebar-placeholder');
-    fetch('../Dashboard/sidebar.html')
+    fetch('../Dashboard/sidebar.php')
         .then(response => response.text())
         .then(data => {
             sidebarPlaceholder.innerHTML = data;
