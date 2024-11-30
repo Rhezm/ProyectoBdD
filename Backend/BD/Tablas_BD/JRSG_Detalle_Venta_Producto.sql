aad6bbc6aeb14805ab5dae311426a2be9b25bd43
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-30 07:45:58 -0300

    Update JRSG_Detalle_Venta_Producto.sql

--- Backend/BD/Tablas_BD/JRSG_Detalle_Venta_Producto.sql
+++ Backend/BD/Tablas_BD/JRSG_Detalle_Venta_Producto.sql
@@ -2,7 +2,6 @@ create table JRSG_Detalle_Venta_Producto ( --- Se actualizo, se agrego cantidad.
     id_venta number,
     id_producto number,
     cantidad number,
-    nombre_producto varchar2(60),
 
     constraint PK_JRSG_Detalle_Venta_Producto primary key (id_producto, id_venta),
     constraint FK_JRSG_Venta2 foreign key (id_venta) references JRSG_Venta (id_venta),
