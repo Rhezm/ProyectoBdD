1665066666a18f49b2944a05f8d67cd8f73e22a5
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-30 07:47:47 -0300

    Update and rename JRSG_Trig_Mayus_Empleados.sql to JRSG_Trig_Init_Empleados.sql

---
+++ Backend/BD/Trigger's/JRSG_Trig_Init_Empleados.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE TRIGGER JRSG_Trig_Mayus_Empleados
+BEFORE INSERT ON JRSG_EMPLEADO 
+FOR EACH ROW
+BEGIN
+    :NEW.nombre_empleado := INITCAP(:NEW.nombre_empleado);
+    :NEW.apellido1_empleado := INITCAP(:NEW.apellido1_empleado);
+    :NEW.apellido2_empleado := INITCAP(:NEW.apellido2_empleado);
+    :NEW.email_empleado := INITCAP(:NEW.email_empleado);
+END;
--- Backend/BD/Trigger's/JRSG_Trig_Mayus_Empleados.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TRIGGER JRSG_Trig_Mayus_Empleados
-BEFORE INSERT ON JRSG_EMPLEADO 
-FOR EACH ROW
-BEGIN
-    :NEW.nombre_empleado := UPPER(:NEW.nombre_empleado);
-    :NEW.apellido1_empleado := UPPER(:NEW.apellido1_empleado);
-    :NEW.apellido2_empleado := UPPER(:NEW.apellido2_empleado);
-    :NEW.email_empleado := UPPER(:NEW.email_empleado);
-END;
