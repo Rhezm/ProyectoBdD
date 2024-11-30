4a8ed263125fd0683be9c4e617d1b6f2664d68d8
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-30 08:11:40 -0300

    Update and rename registros.html to registros.php

--- Frontend/Dashboard/registros.php
+++ Frontend/Dashboard/registros.php
@@ -1,3 +1,4 @@
+<?php include('session.php'); ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -5,11 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comercial Andrades - Registros</title> 
     <link rel="stylesheet" href="..\CSS\estilo_principal.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <div id="sidebar-placeholder"></div>
     <div class="content" id="content"></div>
-    <script src="..\Javascript\registros.js"></script>
+    <script src="..\Javascript\registro.js"></script>
     <script src="..\Javascript\sidebar.js"></script>
 </body>
 </html>
