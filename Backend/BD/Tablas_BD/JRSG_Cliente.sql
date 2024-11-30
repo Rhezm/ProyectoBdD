f433b47d7414758b6e57ee0eb633140a3c68e547
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-30 07:43:55 -0300

    Update JRSG_Cliente.sql

--- Backend/BD/Tablas_BD/JRSG_Cliente.sql
+++ Backend/BD/Tablas_BD/JRSG_Cliente.sql
@@ -4,7 +4,6 @@ create table JRSG_Cliente (
     apellido1_cliente varchar2(20),
     apellido2_cliente varchar2(20),
     telefono_cliente number,
-    direccion_cliente varchar2(100),
     email_cliente varchar2(50),
 
     constraint PK_JRSG_Cliente primary key (id_cliente)
