63224475ffd471042397158f84f0ff4ccc107e91
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-30 01:22:10 -0300

    Update JRSG_Trig_Mayus_Clientes.sql

--- Backend/BD/Trigger's/JRSG_Trig_Mayus_Clientes.sql
+++ Backend/BD/Trigger's/JRSG_Trig_Mayus_Clientes.sql
@@ -2,9 +2,9 @@ CREATE OR REPLACE TRIGGER JRSG_Trig_Mayus_Clientes
 BEFORE INSERT ON JRSG_CLIENTE 
 FOR EACH ROW
 BEGIN
-    :NEW.NOMBRE_CLIENTE := UPPER(:NEW.NOMBRE_CLIENTE);
-    :NEW.apellido1_cliente := UPPER(:NEW.apellido1_cliente);
-    :NEW.apellido2_cliente := UPPER(:NEW.apellido2_cliente);
-    :NEW.direccion_cliente := UPPER(:NEW.direccion_cliente);
-    :NEW.email_cliente := UPPER(:NEW.email_cliente);
+    :NEW.NOMBRE_CLIENTE := INITCAP(:NEW.NOMBRE_CLIENTE);
+    :NEW.apellido1_cliente := INITCAP(:NEW.apellido1_cliente);
+    :NEW.apellido2_cliente := INITCAP(:NEW.apellido2_cliente);
+    :NEW.direccion_cliente := INITCAP(:NEW.direccion_cliente);
+    :NEW.email_cliente := INITCAP(:NEW.email_cliente);
 END;
