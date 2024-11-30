d4efb22e1484f385375ba600a13620a222b019cb
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-30 06:33:12 -0300

    Update Crear_Empleados.sql

--- Backend/BD/Llenar_Tablas/Crear_Empleados.sql
+++ Backend/BD/Llenar_Tablas/Crear_Empleados.sql
@@ -1,6 +1,6 @@
 begin
 	JRSG_Pro_Crear_Empleado('Roberto', 'Barros', 'Gutiérrez', 111111111, 'roberto@gmail.com', 'roberto');
-	JRSG_Pro_Crear_Empleado('Jeison', 'Salas', 'Coloma', 222222222, 'roberto@gmail.com', 'jeison');
-	JRSG_Pro_Crear_Empleado('Sofia', 'Espinosa', 'Solís', 333333333, 'roberto@gmail.com', 'sofia');
-	JRSG_Pro_Crear_Empleado('Gustavo', 'Perez', 'Perez', 444444444, 'roberto@gmail.com', 'gustavo');
+	JRSG_Pro_Crear_Empleado('Jeison', 'Salas', 'Coloma', 222222222, 'jeison@gmail.com', 'jeison');
+	JRSG_Pro_Crear_Empleado('Sofia', 'Espinosa', 'Solís', 333333333, 'sofia@gmail.com', 'sofia');
+	JRSG_Pro_Crear_Empleado('Gustavo', 'Espinoza', 'Fuentes', 444444444, 'gustavo@gmail.com', 'gustavo');
 end;
