88f681862a94e077f680f02670fbaac6e00d05de
Gust <102482818+Rhezm@users.noreply.github.com>
2024-11-30 09:36:29 -0300

    Update cat_mas_vendidas.php

--- Backend/PHP/cat_mas_vendidas.php
+++ Backend/PHP/cat_mas_vendidas.php
@@ -1,12 +1,10 @@
 <?php
-// Configuración de la base de datos
 $host = "localhost";
 $user = "C##usuario";
 $password = "123";
 $service_name = "xe";
 $connection_string = "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$host)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=$service_name)))";
 
-// Conexión a la base de datos
 $conn = oci_connect($user, $password, $connection_string);
 if (!$conn) {
     $e = oci_error();
@@ -14,14 +12,12 @@ if (!$conn) {
     exit;
 }
 
-// Llamar al procedimiento almacenado
 $stid = oci_parse($conn, "BEGIN jrsg_pro_categorias_mas_vendidas(:p_cursor); END;");
 $cursor = oci_new_cursor($conn);
 oci_bind_by_name($stid, ":p_cursor", $cursor, -1, OCI_B_CURSOR);
 oci_execute($stid);
 oci_execute($cursor);
 
-// Generar filas de la tabla
 $filas = '';
 while (($row = oci_fetch_array($cursor, OCI_ASSOC + OCI_RETURN_NULLS)) != false) {
     $filas .= "<tr> <td class='id_producto'>{$row['A']}</td>
